9a50715d387405e9d659d8c4566a6ed784bff0a7
Maria Fernanda Barçante <barcantef@gmail.com>
2023-07-14 11:40:45 -0300

    tipos primitivos

---
+++ exemplos/aula06/TiposPrimitivos.py
@@ -0,0 +1,8 @@
+nome = input('Olá, digite seu nome: ')
+n1 = int(input('Agora digite um número: '))
+n2 = int(input('Digite mais um número: '))
+s = n1 + n2
+
+print('A soma vale: ', s)
+print('Olá {}, a soma vale: {}'.format(nome,s))
+
