6a7709f477e8b9366aaf16965af1fc247f29f3c2
Maria Fernanda Barçante <barcantef@gmail.com>
2023-08-14 18:31:38 -0300

    variaveis

--- exemplos/aula02/entrada-e-saida.py
+++ exemplos/aula02/entrada-e-saida.py
@@ -1,2 +1,19 @@
-print('Oi mundo! \nSegundo print')
 # \n faz uma quebra de linha
+print('Oi mundo! \nSegundo print')
+
+# variaveis
+nome = 'Fernanda'
+idade = 19
+altura = 1.70
+print('Ola ' + nome)
+
+# retorna o tipo da variavel
+tipo_nome = type(nome)
+tipo_idade = type(idade)
+tipo_altura = type(altura)
+print(tipo_nome)
+print(tipo_idade)
+print(tipo_altura)
+
+#é mais facil concatenar com virgula
+print(nome, 'tem', idade, 'anos')
---
+++ exemplos/aula02/input.py
@@ -0,0 +1,5 @@
+nome = input('Digite o seu nome: ')
+idade = int(input('Digite a sua idade: '))
+altura = float(input('Digite a sua altura: '))
+
+print(type(idade), type(nome), type(altura))
