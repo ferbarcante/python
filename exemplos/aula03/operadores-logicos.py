e18824995888474227c30e8812060e188fbd0193
Maria Fernanda Barçante <barcantef@gmail.com>
2023-08-15 16:20:35 -0300

    if, else, elif

---
+++ exemplos/aula03/operadores-logicos.py
@@ -0,0 +1,15 @@
+
+var_verdade = True
+var_falso = False
+
+if var_verdade == True:
+    print('var_verdade é verdadeiro', var_verdade)
+else:
+    print('var_verdade é falso')
+
+
+a = int(input('Digite um numero:'))
+if a>2:
+    print(a, 'é maior que dois')
+else:
+    print(a, 'não é maior que dois')
